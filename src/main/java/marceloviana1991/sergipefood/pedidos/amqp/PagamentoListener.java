25c3b243704627f1973cd6331ae8eeadb1d26ac8
marcelo <marceloviana1991@gmail.com>
2024-11-29 07:44:17 -0300

    Update Mensageria

---
+++ src/main/java/marceloviana1991/sergipefood/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,18 @@
+package marceloviana1991.sergipefood.pedidos.amqp;
+
+import marceloviana1991.sergipefood.pedidos.service.PedidoService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagamentoListener {
+
+    @Autowired
+    private PedidoService service;
+
+    @RabbitListener(queues = "pagamento.concluido")
+    public void recebeMensagem(Long id) {
+        service.aprovaPagamento(id);
+    }
+}
